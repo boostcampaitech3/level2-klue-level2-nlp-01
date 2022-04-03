51240b6629943d91e779584ca2537a8704724304
leeyejin <ssgyeijn@g.skku.edu>
2022-04-03 19:49:45 +0000

    addig fold

--- train.py
+++ train.py
@@ -1,15 +1,18 @@
 import pickle as pickle
 import os
+from random import shuffle
 import pandas as pd
 import torch
 import sklearn
 import numpy as np
 from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score
+from sklearn.model_selection import StratifiedKFold
 from transformers import ElectraModel, ElectraTokenizer, AutoTokenizer, AutoConfig, AutoModelForSequenceClassification, Trainer, TrainingArguments, RobertaConfig, RobertaTokenizer, RobertaForSequenceClassification, BertTokenizer
 from load_data import *
 import wandb
+import argparse
 
-wandb.init(project="nlp_level2", entity="ssgyejin")
+# wandb.init(project="nlp_level2", entity="ssgyejin")
 
 def klue_re_micro_f1(preds, labels):
     """KLUE-RE micro f1 (except no_relation)"""
@@ -51,6 +54,7 @@ def compute_metrics(pred):
   f1 = klue_re_micro_f1(preds, labels)
   auprc = klue_re_auprc(probs, labels)
   acc = accuracy_score(labels, preds) # 리더보드 평가에는 포함되지 않습니다.
+  wandb.log({"micro f1": f1, "auprc": auprc, "acc": acc})
 
   return {
       'micro f1 score': f1,
@@ -67,75 +71,122 @@ def label_to_num(label):
   
   return num_label
 
-def train():
+def train(args):
   # load model and tokenizer
   # MODEL_NAME = "bert-base-uncased"
   # MODEL_NAME = "klue/bert-base"
-  MODEL_NAME = "monologg/koelectra-base-v3-discriminator"
-  # tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
-  tokenizer = ElectraTokenizer.from_pretrained(MODEL_NAME)
+  # MODEL_NAME = "monologg/koelectra-base-v3-discriminator"
+  MODEL_NAME = "klue/roberta-large"
+  tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+  # tokenizer = ElectraTokenizer.from_pretrained(MODEL_NAME)
 
   # load dataset
-  train_dataset = load_data("~/dataset/train/train.csv")
+  train_dataset = load_data("~/dataset/train/alternate_train.csv")
+  # train_dataset, dev_dataset = split_train_valid_stratified(train_dataset, split_ratio=0.2)
   # dev_dataset = load_data("../dataset/train/dev.csv") # validation용 데이터는 따로 만드셔야 합니다.
 
   train_label = label_to_num(train_dataset['label'].values)
   # dev_label = label_to_num(dev_dataset['label'].values)
 
   # tokenizing dataset
-  tokenized_train = tokenized_dataset(train_dataset, tokenizer)
+  # tokenized_train = tokenized_dataset(train_dataset, tokenizer)
   # tokenized_dev = tokenized_dataset(dev_dataset, tokenizer)
 
   # make dataset for pytorch.
-  RE_train_dataset = RE_Dataset(tokenized_train, train_label)
+  # RE_train_dataset = RE_Dataset(tokenized_train, train_label)
   # RE_dev_dataset = RE_Dataset(tokenized_dev, dev_label)
 
   device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
   print(device)
-  # setting model hyperparameter
-  model_config =  AutoConfig.from_pretrained(MODEL_NAME)
-  model_config.num_labels = 30
-
-  model =  AutoModelForSequenceClassification.from_pretrained(MODEL_NAME, config=model_config)
-  print(model.config)
-  model.parameters
-  model.to(device)
   
-  # 사용한 option 외에도 다양한 option들이 있습니다.
-  # https://huggingface.co/transformers/main_classes/trainer.html#trainingarguments 참고해주세요.
-  training_args = TrainingArguments(
-    output_dir='./results',          # output directory
-    save_total_limit=5,              # number of total save model.
-    save_steps=500,                 # model saving step.
-    num_train_epochs=10,              # total number of training epochs
-    learning_rate=5e-4,               # learning_rate
-    per_device_train_batch_size=32,  # batch size per device during training
-    per_device_eval_batch_size=32,   # batch size for evaluation
-    warmup_steps=500,                # number of warmup steps for learning rate scheduler
-    weight_decay=0.01,               # strength of weight decay
-    logging_dir='./logs',            # directory for storing logs
-    logging_steps=100,              # log saving step.
-    evaluation_strategy='steps', # evaluation strategy to adopt during training
-                                # `no`: No evaluation during training.
-                                # `steps`: Evaluate every `eval_steps`.
-                                # `epoch`: Evaluate every end of epoch.
-    eval_steps = 500,            # evaluation step.
-    load_best_model_at_end = True 
+  n_splits = args.num_splits #num of fold
+  
+  startifiedFold = StratifiedKFold(
+    n_splits=n_splits, shuffle=True, random_state=42
   )
-  trainer = Trainer(
-    model=model,                         # the instantiated 🤗 Transformers model to be trained
-    args=training_args,                  # training arguments, defined above
-    train_dataset=RE_train_dataset,         # training dataset
-    eval_dataset=RE_train_dataset,             # evaluation dataset
-    compute_metrics=compute_metrics         # define metrics function
-  )
+  
+  for fold_index, (train_index, eval_index) in enumerate(startifiedFold.split(X=train_dataset, y=train_label)):
+    print('Train start {}/{} Fold'.format(fold_index+1, n_splits))
+        
+    # fold_dataset, eval_dataset
+    fold_dataset = train_dataset.iloc[train_index]
+    fold_label = [train_label[i] for i in train_index]
 
-  # train model
-  trainer.train()
-  model.save_pretrained('./best_model')
-def main():
-  train()
+    eval_dataset = train_dataset.iloc[eval_index]
+    eval_label = [train_label[i] for i in eval_index]
+
+    # tokenizing dataset
+    tokenized_train = tokenized_dataset(fold_dataset, tokenizer)
+    tokenized_dev = tokenized_dataset(eval_dataset, tokenizer)
+
+    # make dataset for pytorch.
+    RE_train_dataset = RE_Dataset(tokenized_train, fold_label)
+    RE_dev_dataset = RE_Dataset(tokenized_dev, eval_label)
+
+
+  
+    # setting model hyperparameter
+    model_config =  AutoConfig.from_pretrained(MODEL_NAME)
+    model_config.num_labels = 30
+
+    model =  AutoModelForSequenceClassification.from_pretrained(MODEL_NAME, config=model_config)
+    print(model.config)
+    model.parameters
+    model.to(device)
+    
+    # 사용한 option 외에도 다양한 option들이 있습니다.
+    # https://huggingface.co/transformers/main_classes/trainer.html#trainingarguments 참고해주세요.
+    training_args = TrainingArguments(
+      output_dir=f'./results/{fold_index}',          # output directory
+      save_total_limit=5,              # number of total save model.
+      save_steps=500,                 # model saving step.
+      num_train_epochs=10,              # total number of training epochs
+      learning_rate=1e-5,               # learning_rate
+      per_device_train_batch_size=32,  # batch size per device during training
+      per_device_eval_batch_size=32,   # batch size for evaluation
+      warmup_steps=500,                # number of warmup steps for learning rate scheduler
+      weight_decay=0.01,               # strength of weight decay
+      logging_dir='./logs',            # directory for storing logs
+      logging_steps=100,              # log saving step.
+      evaluation_strategy='steps', # evaluation strategy to adopt during training
+                                  # `no`: No evaluation during training.
+                                  # `steps`: Evaluate every `eval_steps`.
+                                  # `epoch`: Evaluate every end of epoch.
+      eval_steps = 500,            # evaluation step.
+      load_best_model_at_end = True,
+      report_to = ['wandb'],
+      metric_for_best_model = 'micro f1 score'
+      
+    )
+    trainer = Trainer(
+      model=model,                         # the instantiated 🤗 Transformers model to be trained
+      args=training_args,                  # training arguments, defined above
+      train_dataset=RE_train_dataset,         # training dataset
+      eval_dataset=RE_dev_dataset,             # evaluation dataset
+      compute_metrics=compute_metrics         # define metrics function
+    )
+
+    # train model
+    trainer.train()
+    model.save_pretrained('./best_model')
+    print(f'------{fold_index+1} finished------')
+  
+  print('======Finsh train======')
+    
+def main(args):
+  train(args)
 
 if __name__ == '__main__':
-  main()
+  os.environ['WANDB_API_KEY'] = '474ad8c49db45ffa138dcae91410d5a2ff4ffad1'
+  os.environ['WANDB_ENTITY'] = 'plzanswer'
+  os.environ['WANDB_PROJECT'] = 'test-project'
+  os.environ['WANDB_LOG_MODEL'] = 'Ture'
+  
+  parser = argparse.ArgumentParser()
+  
+  # model dir
+  parser.add_argument('--n_splits', type=int, default=5)
+  args = parser.parse_args()
+  
+  main(args)
