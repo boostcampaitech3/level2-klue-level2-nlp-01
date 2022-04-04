64434babf137421dcf37b0afa0836988979bd9b6
leeyejin <ssgyeijn@g.skku.edu>
2022-04-04 07:39:01 +0000

    modify error, data augmentation

--- load_data.py
+++ load_data.py
@@ -22,7 +22,7 @@ class RE_Dataset(torch.utils.data.Dataset):
   def __len__(self):
     return len(self.labels)
 
-def preprocessing_dataset(dataset):
+def preprocessing_dataset(dataset, state):
   """ 처음 불러온 csv 파일을 원하는 형태의 DataFrame으로 변경 시켜줍니다."""
   subject_entity = []
   object_entity = []
@@ -33,31 +33,44 @@ def preprocessing_dataset(dataset):
     subject_entity.append(i)
     object_entity.append(j)
   out_dataset = pd.DataFrame({'id':dataset['id'], 'sentence':dataset['sentence'],'subject_entity':subject_entity,'object_entity':object_entity,'label':dataset['label'],})
-  
-  augmented = make_augmented(out_dataset, 'swap', len(out_dataset))
-  
-  augmented_data = pd.concat([out_dataset, augmented])
-  
-  augmented2 = make_augmented(out_dataset, 'delete', len(augmented_data))
-  
-  augmented_data = pd.concat([augmented_data, augmented2])
-  
+
+  if state == 'train':
+    print("==== start augmentation ====")
+    augmented = make_augmented(out_dataset, 'swap', len(out_dataset))
+    print("===== augmented sentence length(swap) : ", len(augmented), " =====")
+    augmented_data = pd.concat([out_dataset, augmented])
+
+    augmented2 = make_augmented(out_dataset, 'delete', len(augmented_data))
+    print("===== augmented sentence length(delete) : ", len(augmented2), " =====")
+    augmented_data = pd.concat([augmented_data, augmented2])  
+    print("==== end augmentation ====")
+  else:
+    augmented_data = out_dataset.copy()
   
   return augmented_data
+  
+  
+  # return out_dataset
 
 def make_augmented(dataset, eda, num):
-  augmented = []
-  for i in range(len(dataset)):
-    augmented_sentences = []
-    augmented_sentences = EDA(sentence=dataset['sentence'][i], subject=dataset['subject_entity'][i], object=['object_entity'][i], what_eda=eda)
-    augmented.loc[i] = [num+i, augmented_sentences, dataset['subject_eintity'][i], dataset['object_entity'][i], dataset['label'][i]]
+  augmented = pd.DataFrame(columns=['id', 'sentence', 'subject_entity', 'object_entity', 'label'])
+  id = num
+  for i in range(10000):
+    print(i,"/", 10000)
+    if dataset['label'][i] != 'no_relation':
+      augmented_sentences = ''
+      # print("====", dataset['sentence'][i])
+      augmented_sentences = EDA(sentence=dataset['sentence'][i], subject=dataset['subject_entity'][i], object=dataset['object_entity'][i], what_eda=eda)
+      # print("setence : ", augmented_sentences)
+      augmented.loc[i] = [id, augmented_sentences, dataset['subject_entity'][i], dataset['object_entity'][i], dataset['label'][i]]
+      id += 1
     
-    return augmented
+  return augmented
 
-def load_data(dataset_dir):
+def load_data(dataset_dir, state):
   """ csv 파일을 경로에 맡게 불러 옵니다. """
   pd_dataset = pd.read_csv(dataset_dir, sep='\t')
-  dataset = preprocessing_dataset(pd_dataset)
+  dataset = preprocessing_dataset(pd_dataset, state)
   
   return dataset
 
@@ -136,7 +149,7 @@ def swap_word(new_words):
 def EDA(sentence, subject, object, what_eda, alpha_rs = 0.1, p_rd = 0.15):
   # sentence = get_only_hangul(sentence)
   words = sentence.split(' ')
-  words = [words for word in words if word is not ""]
+  # print("words : ", words)
   num_words = len(words)
   
   n_rs = max(1, int(alpha_rs * num_words))
@@ -148,8 +161,14 @@ def EDA(sentence, subject, object, what_eda, alpha_rs = 0.1, p_rd = 0.15):
   #random delete, subject와 object에 있는 것은 삭제하면 안됨!
   if what_eda == 'delete':
     augmented_sentences = random_deletion(words, subject, object, p_rd)
+    
+  for i in range(len(augmented_sentences)):
+    augmented_sentences[i] = str(augmented_sentences[i])
+  
+  augmented_str = " ".join(augmented_sentences)
+  # print("make sentence : ", augmented_str)
 
-  return ' '.join(augmented_sentences)
+  return augmented_str
   
   
   
