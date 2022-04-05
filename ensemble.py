e9a80d5c00a8b1a2dad763a84951d86c6633d108
Kang-Beom-Seo <bumsur@hanmail.net>
2022-04-05 17:16:59 +0000

    StackingEnsembleFirstStep Added

--- ensemble.py
+++ ensemble.py
@@ -1,9 +1,16 @@
+# KBS) 아래의 두 링크의 내용을 읽어보고, 그를 토대로 짜보는 앙상블 클래스입니다.
+# KBS) http://rasbt.github.io/mlxtend/user_guide/classifier/StackingCVClassifier/
+# KBS) https://techblog-history-younghunjo1.tistory.com/103
 import numpy as np
 import pandas as pd
+import torch
+import random
+import os
 from typing import List, Callable, Iterable
 from sklearn.model_selection import StratifiedKFold
 from utils import *
 
+# KBS) 전체 데이터셋을 우선적으로 train_dataset 및 test_dataset으로 나누고, 여기서 train_dataset을 다시 KFold로 분리한다.
 def KFold_split_dataset(dataset: Iterable, num_folds: int=5, split_ratio: float=0.2):
     train_dataset, test_dataset = split_train_valid_stratified(dataset, split_ratio=split_ratio)
     train_label = np.array(label_to_num(train_dataset['label'].values))
@@ -21,21 +28,92 @@ def KFold_split_dataset(dataset: Iterable, num_folds: int=5, split_ratio: float=
     
     return kfold_train_dataset, kfold_train_label, kfold_valid_dataset, kfold_valid_label, test_dataset, test_label
 
+# KBS) 먼저, 1단계로 각 모델에 대해 KFold로 각각의 모델(base classifier)을 개별적으로 학습시킨다.
 class StackingEnsembleFirstStep:
-    def __init__(self, dataset: Iterable, tokenizer_list: List[Callable], model_list: List[Callable], num_folds:int = 5):
-        train_dataset, test_dataset = split_train_valid_stratified(dataset, split_ratio=0.2)
-        train_label = label_to_num(train_dataset['label'].values)
-        test_label = label_to_num(test_dataset['label'].values)
-        skf = StratifiedKFold(n_splits=num_folds)
-        kfold_train_dataset = []
-        kfold_valid_dataset = []
-        kfold_train_label = []
-        kfold_valid_label = []
-        for train_idx, valid_idx in skf.split(train_dataset, train_label):
-            kfold_train_dataset.append(train_dataset[train_idx])
-            kfold_train_label.append(train_label[train_idx])
-            kfold_valid_dataset.append(train_dataset[valid_idx])
-            kfold_valid_label.append(train_label[valid_idx])
+    def __init__(self, kfold_train_dataset: List[Iterable], kfold_train_label: List[Iterable],
+                       kfold_valid_dataset: List[Iterable], kfold_valid_label: List[Iterable]):
+        assert len(kfold_train_dataset) == len(kfold_valid_dataset), 'train_dataset 1개당 valid_dataset 1개를 맞추어주세요.'
+        assert len(kfold_train_dataset) == len(kfold_train_label), 'train_dataset과 train_label 개수를 맞추어주세요.'
+        assert len(kfold_valid_dataset) == len(kfold_valid_label), 'valid_dataset과 valid_label 개수를 맞추어주세요.'
+        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        self.kfold_train_dataset = kfold_train_dataset
+        self.kfold_train_label = kfold_train_label
+        self.kfold_valid_dataset = kfold_valid_dataset
+        self.kfold_valid_label = kfold_valid_label
+        self.tokenizer_list = []
+        self.model_list = []
+        self.training_arguments_list = []
+        self.trainerClass_list = []
+        self.tokenizedClass_list = []
+        self.REClass_list = []
+        self.compute_metrics_list = []
+
+    def train(self, seed_value=42):
+        self.set_seed(seed_value=seed_value)
+        for idx, (train_dataset, train_label, valid_dataset, valid_label) in enumerate(zip(self.kfold_train_dataset, self.kfold_train_label, self.kfold_valid_dataset, self.kfold_valid_label)):
+            for idss, (tokenizer, model, training_arguments, trainerClass, tokenizedClass, REClass, compute_metrics) in enumerate(zip(self.tokenizer_list, self.model_list,
+                                                                                                                                      self.training_arguments_list, self.trainerClass_list,
+                                                                                                                                      self.tokenizedClass_list, self.REClass_list,
+                                                                                                                                      self.compute_metrics_list)):
+                # tokenizing dataset
+                tokenized_train = tokenizedClass(train_dataset, tokenizer)
+                tokenized_valid = tokenizedClass(valid_dataset, tokenizer)
+
+                # make dataset for pytorch.
+                RE_train_dataset = REClass(tokenized_train, train_label)
+                RE_valid_dataset = REClass(tokenized_valid, valid_label)
+                model.to(self.device)
+                trainer = trainerClass(model=model,                         
+                                       args=training_arguments,                 
+                                       train_dataset=RE_train_dataset,         
+                                       eval_dataset=RE_valid_dataset,            
+                                       compute_metrics=compute_metrics)
+                trainer.train()
+                output_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'step1_models', str(idss) + '번모델', str(idx) + '-KFold')
+                os.makedirs(output_path, exist_ok=True)
+                model.save_pretrained(output_path)
+                tokenizer.save_pretrained(output_path)
+                eval_dataloader = trainer.get_eval_dataloader()
+                predictions = []
+                labels_list = []
+
+                model.eval()
+
+                for _, inputs in enumerate(eval_dataloader):
+                    print(inputs)
+                    inputs = inputs.to(self.device)
+                    labels = inputs.get("labels")
+                    outputs = model(**inputs)
+                    logits = outputs.get("logits")
+                    predictions.append(logits.detach().cpu().numpy())
+                    labels_list.append(labels.detach().cpu().numpy())
+
+                predictions = np.concatenate(predictions, dim=0)
+                print(labels_list)
+                second_step_inputs = {'predictions' : predictions, 'labels' : labels_list}
+                with open(os.path.join(output_path, 'predictions.pkl'), 'wb') as f:
+                    pickle.dump(predictions, f)
+
+                print('\n\n\n')
+                print('-' * 30)
+                print(f'{idx}-Fold | {idss}번째 Model 완료!')
+                print('-' * 30)
+
+    
+    def add_arguments(self, tokenizer, model, training_arguments, trainerClass:Callable, tokenizedClass, REClass, compute_metrics):
+        self.tokenizer_list.append(tokenizer)
+        self.model_list.append(model)
+        self.training_arguments_list.append(training_arguments)
+        self.trainerClass_list.append(trainerClass)
+        self.tokenizedClass_list.append(tokenizedClass)
+        self.REClass_list.append(REClass)
+        self.compute_metrics_list.append(compute_metrics)
+    
+    def set_seed(self, seed_value=42):
+        random.seed(seed_value)
+        np.random.seed(seed_value)
+        torch.manual_seed(seed_value)
+        torch.cuda.manual_seed_all(seed_value)
 
 class StackingEnsembleSecondStep:
     def __init__(self):
@@ -56,5 +134,4 @@ if __name__ == '__main__':
     print(len(kfold_train_dataset[0]), len(kfold_train_label[0]))
     print(len(kfold_valid_dataset[0]), len(kfold_valid_label[0]))
     print(len(test_dataset), len(test_label))
-    #StackingEnsembleFirstStep()
     print('Finished!')
--- train.py
+++ train.py
@@ -7,6 +7,7 @@ import numpy as np
 from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score
 from transformers import AutoTokenizer, AutoConfig, AutoModelForSequenceClassification, Trainer, TrainingArguments, RobertaConfig, RobertaTokenizer, RobertaForSequenceClassification, BertTokenizer
 from utils import *
+from ensemble import *
 from ray import tune
 from models import *
 import wandb
@@ -14,43 +15,32 @@ import copy
 import random
 
 def train():
-  # load model and tokenizer
-  # load dataset
-  # train_dataset = load_data("./dataset/train/train.csv")
-  # KBS) dataset 변경
   set_seed(42)
   device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
   tokenizer, model = TokenizerAndModelForKlueReTask()
-  train_dataset = load_data("./dataset/train/alternate_train copy.csv")
-  train_dataset, dev_dataset = split_train_valid_stratified(train_dataset, split_ratio=0.2)
-
-  train_label = label_to_num(train_dataset['label'].values)
-  dev_label = label_to_num(dev_dataset['label'].values)
-
-  # tokenizing dataset
-  tokenized_train = tokenized_dataset(train_dataset, tokenizer)
-  tokenized_dev = tokenized_dataset(dev_dataset, tokenizer)
-
-  # make dataset for pytorch.
-  RE_train_dataset = RE_Dataset(tokenized_train, train_label)
-  RE_dev_dataset = RE_Dataset(tokenized_dev, dev_label)
-
+  tokenizer2, model2 = TokenizerAndModelForKlueReTask()
+  with open('./train_dataframe.pkl', 'rb') as f:
+    dataset = pickle.load(f)
+  
   print(device)
+
+  kfold_train_dataset, kfold_train_label, kfold_valid_dataset, kfold_valid_label, test_dataset, test_label = KFold_split_dataset(dataset, num_folds=5, split_ratio=0.2)
+  ensemble_first_step = StackingEnsembleFirstStep(kfold_train_dataset, kfold_train_label, kfold_valid_dataset, kfold_valid_label)
   
   # 사용한 option 외에도 다양한 option들이 있습니다.
   # https://huggingface.co/transformers/main_classes/trainer.html#trainingarguments 참고해주세요.
   training_args = TrainingArguments(
-    output_dir='/opt/ml/code/level2-klue-level2-nlp-01/results',          # output directory
+    output_dir='./results',          # output directory
     save_total_limit=5,
-    num_train_epochs=3,              # total number of training epochs
+    num_train_epochs=1,              # total number of training epochs
     learning_rate=3e-05,               # learning_rate
-    gradient_accumulation_steps=2,
+    gradient_accumulation_steps=1,
     save_strategy='steps',
     per_device_train_batch_size=32,  # batch size per device during training
     per_device_eval_batch_size=32,   # batch size for evaluation
     warmup_steps=500,                # number of warmup steps for learning rate scheduler
     weight_decay=0.01,               # strength of weight decay
-    logging_dir='/opt/ml/code/level2-klue-level2-nlp-01/logs',            # directory for storing logs
+    logging_dir='./logs',            # directory for storing logs
     logging_steps=100,              # log saving step.
     evaluation_strategy='steps', # evaluation strategy to adopt during training
     eval_steps = 500,            # evaluation step.
@@ -60,34 +50,34 @@ def train():
     greater_is_better=True
   )
 
-  trainer = ImbalancedSamplerTrainer(
-    model=model,                         
-    args=training_args,                 
-    train_dataset=RE_train_dataset,         
-    eval_dataset=RE_dev_dataset,            
-    compute_metrics=compute_metrics      
+  training_args2 = TrainingArguments(
+    output_dir='./results',          # output directory
+    save_total_limit=5,
+    num_train_epochs=1,              # total number of training epochs
+    learning_rate=2e-05,               # learning_rate
+    gradient_accumulation_steps=2,
+    save_strategy='steps',
+    per_device_train_batch_size=32,  # batch size per device during training
+    per_device_eval_batch_size=32,   # batch size for evaluation
+    warmup_steps=500,                # number of warmup steps for learning rate scheduler
+    weight_decay=0.01,               # strength of weight decay
+    logging_dir='./logs',            # directory for storing logs
+    logging_steps=100,              # log saving step.
+    evaluation_strategy='steps', # evaluation strategy to adopt during training
+    eval_steps = 500,            # evaluation step.
+    load_best_model_at_end = True,
+    report_to=['wandb'],
+    metric_for_best_model='micro f1 score',
+    greater_is_better=True
   )
-
-  # trainer = Trainer(
-  #    model=model,                       
-  #    args=training_args,                
-  #    train_dataset=RE_train_dataset,       
-  #    eval_dataset=RE_dev_dataset,          
-  #    compute_metrics=compute_metrics      
-  #  )
-
-  # train model
-  # best_hyperparameter = trainer.hyperparameter_search(
-  #   direction="maximize", 
-  #   backend="ray", 
-  #   hp_space=hp_space_ray,
-  #   compute_objective=compute_objective
-  #   )
-  trainer.train()
-  # with open('/opt/ml/code/level2-klue-level2-nlp-01/best_model/best_hyperparameter.pkl', 'wb') as f:
-  #     pickle.dump(best_parameter, f)
-  model.save_pretrained('./best_model')
-  tokenizer.save_pretrained('./best_model')
+  ensemble_first_step.add_arguments(tokenizer, model, training_args, ImbalancedSamplerTrainer,
+                                    tokenized_dataset, RE_Dataset, compute_metrics)
+  ensemble_first_step.add_arguments(tokenizer2, model2, training_args2, ImbalancedSamplerTrainer,
+                                    tokenized_dataset, RE_Dataset, compute_metrics)
+  ensemble_first_step.train(seed_value=42)
+  #model.save_pretrained('./best_model')
+  #tokenizer.save_pretrained('./best_model')
+  print('Finished!')
 
 def set_seed(seed_value=42):
     """Set seed for reproducibility.
@@ -97,30 +87,30 @@ def set_seed(seed_value=42):
     torch.manual_seed(seed_value)
     torch.cuda.manual_seed_all(seed_value)
 
-# def model_init():
-#     model_config =  AutoConfig.from_pretrained(MODEL_NAME)
-#     model_config.num_labels = 30
+'''def model_init():
+    model_config =  AutoConfig.from_pretrained(MODEL_NAME)
+    model_config.num_labels = 30
 
-#     model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME, config=model_config)
-#     # KBS) add special tokens
-#     model.resize_token_embeddings(tokenizer.vocab_size + added_token_num)
-#     print(model.parameters)
-#     model.to(device)
+    model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME, config=model_config)
+    # KBS) add special tokens
+    model.resize_token_embeddings(tokenizer.vocab_size + added_token_num)
+    print(model.parameters)
+    model.to(device)
 
-#     return model
+    return model
 
-# def hp_space_ray(trial):
-#     config = {
-#         "learning_rate" : tune.loguniform(1e-5, 5e-5),
-#         "num_train_epochs" : tune.choice(range(3, 15)),
-#         "per_device_train_batch_size" : tune.choice([16, 32]),
-#         "gradient_accumulation_steps" : tune.choice(range(1, 3))
-#     }
-#     return config
+def hp_space_ray(trial):
+    config = {
+        "learning_rate" : tune.loguniform(1e-5, 5e-5),
+        "num_train_epochs" : tune.choice(range(3, 15)),
+        "per_device_train_batch_size" : tune.choice([16, 32]),
+        "gradient_accumulation_steps" : tune.choice(range(1, 3))
+    }
+    return config
 
-# def compute_objective(metrics):
-#     f1 = metrics["eval_micro f1 score"]
-#     return f1
+def compute_objective(metrics):
+    f1 = metrics["eval_micro f1 score"]
+    return f1'''
 
 if __name__ == '__main__':
   os.environ['WANDB_API_KEY'] = 'f5b1f2d16ad90a4bfefca9e344309d152509ac3b'
